123b8e05e64e2714cf08db13eb5ba61381226f4f
Yash Mittal <yashmittal@Yashs-MacBook-Pro.local>
2025-11-19 14:44:00 -0800

    final fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import "../styles/global.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/vicus-dark.png" media="(prefers-color-scheme: light)" />
+        <link rel="icon" href="/vicus-light.png" media="(prefers-color-scheme: dark)" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
